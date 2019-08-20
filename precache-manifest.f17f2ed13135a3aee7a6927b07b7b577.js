defe1241c39cf5c3d4c007d672e449c9d2388261
Harry Chung <hc9825@gmail.com>
2019-08-19 20:01:44 -0700

    Updates

---
+++ precache-manifest.f17f2ed13135a3aee7a6927b07b7b577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c31e2dfd571cdd83a55260613ca75d",
+    "url": "/coachbot-client/index.html"
+  },
+  {
+    "revision": "694703997a6401a98164",
+    "url": "/coachbot-client/static/css/main.143ba070.chunk.css"
+  },
+  {
+    "revision": "efe07f4f95ee2c1791f2",
+    "url": "/coachbot-client/static/js/2.858a7051.chunk.js"
+  },
+  {
+    "revision": "694703997a6401a98164",
+    "url": "/coachbot-client/static/js/main.769b2c58.chunk.js"
+  },
+  {
+    "revision": "224bf7761e88a7f64d99",
+    "url": "/coachbot-client/static/js/runtime~main.62f3bb67.js"
+  }
+]);
